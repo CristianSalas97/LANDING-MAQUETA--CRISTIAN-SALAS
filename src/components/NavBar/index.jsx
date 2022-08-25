337771a3cfec17a39bd127e5eb9e739ecb6946d2
Crhis97 <cristiansalas167@gmail.com>
2022-08-24 22:19:29 -0300

    subida

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import CartWidget from "../CartWidget";
+import { NavLink } from "react-router-dom";
+
+const NavBar = () => {
+	return (
+		<div className="container">
+			<nav className="nav">
+				<div className="nav__brand">
+					<NavLink className="nav__link" to="/">
+						PROCESADOR
+					</NavLink>
+				</div>
+				<ul className="nav__list">
+					<li>
+						<NavLink className="nav__link" to="/categoria/films">
+							COMPONENTES
+						</NavLink>
+					</li>
+					<li>
+						<NavLink className="nav__link" to="/categoria/series">
+							MOTHERBOARDS
+						</NavLink>
+					</li>
+					<li>
+						<NavLink className="nav__link" to="cart">
+							<CartWidget />
+						</NavLink>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	);
+};
+
+export default NavBar;
+
